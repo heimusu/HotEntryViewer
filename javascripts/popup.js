5953fb04cf0948a21d69284ad9499ee7f3f29482
heimusu <heimusu@gmail.com>
2014-01-20 14:12:09 +0900

    deadlock

--- javascripts/popup.js
+++ javascripts/popup.js
@@ -1,6 +1,6 @@
 var getHotEntry;
 var getLog;
-
+var getHotEntry2;
 
 getHotEntry = function() {
   return $.ajax({
@@ -9,12 +9,32 @@ getHotEntry = function() {
     success: function(data) {
       return $(data).find("item").each(function() {
         var title;
+	var  link;
+	link = $(this).find("link").text();
         title = $(this).find("title").text();
-        return $(".feeds").append("<p>" + title + "</p>");
+        //return $(".feeds").append("<p>" + title + "</p>");
+	return $(".feeds").append("<A href = " + link + ">" + title + "</A>");
       });
     }
   });
 };
+
+
+getHotEntry2 = function(){
+    return $.ajax({
+	url: "http://feeds.feedburner.com/hatena/b/hotentry",
+	dataType: "xml",
+	success: function(data){
+	    return $(data).find("item").each(function() {
+		var  link;
+		link = $(this).find("link").text();
+		return $(".feeds").append("<p>" + link + "</p>");
+	    });
+	}
+    });
+};
+
+
 
 getLog = function(){
     return $.ajax({
@@ -26,8 +46,10 @@ getLog = function(){
     });
 };
 
+getLog();
+
 $("button").on("click",function(){
-    return getLog();
+    return getHotEntry();
 });
 
 /*
