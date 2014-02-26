9e40bce3afef0973589282de322a3518cd636b13
Arc Cosine <m.ksdcom@gmail.com>
2014-02-26 23:36:51 +0900

    Add Sort Code. Sort function.Style fix. Remove Japanese comment.

--- javascripts/popup.js
+++ javascripts/popup.js
@@ -1,19 +1,22 @@
-var getHotEntry;
-var getLog;
 var createTab;
 
-getHotEntry = function() {
+function getHotEntry() {
   return $.ajax({
     url: "http://feeds.feedburner.com/hatena/b/hotentry",
     dataType: "xml",
     success: function(data) {
+      $(".feeds").empty();
       return $(data).find("item").each(function() {
-        var title;
-        var  link;
-        link = $(this).find("link").text();
-        title = $(this).find("title").text();
-        //return $(".feeds").append("<p>" + title + "</p>");
-        $("#article-tmpl").tmpl({link: link, title: title}).appendTo(".feeds");
+        var link = $(this).find("link").text();
+        var title = $(this).find("title").text();
+        var hateb = "";
+        $(this).children().each(function() {
+          if ($(this)[0].tagName == "hatena:bookmarkcount") {
+            hateb = $(this).text();
+          }
+        });
+
+        $("#article-tmpl").tmpl({link: link, title: title, hateb: hateb }).appendTo(".feeds");
         $("a:last").on("click", createTab);
       });
     }
@@ -21,7 +24,7 @@ getHotEntry = function() {
 };
 
 
-getLog = function(){
+function getLog(){
   return $.ajax({
     url:"http://feeds.feedburner.com/hatena/b/hotentry",
     dataType:"xml",
@@ -31,23 +34,72 @@ getLog = function(){
   });
 };
 
-createTab = function(event){
+function createTab (event){
   event.preventDefault();
   var $target = $(event.target);
   return chrome.tabs.create({url: $target.attr("href")});
 };
 
 
-/*実行部*/
-getLog();
-getHotEntry();
+// Sort node func add to jQuery.
+$.fn.eachsort = function(callback) {
+    return this.each(function(){
+        return $(this).html(
+            $(this).children().sort(callback)
+        );
+    });
+}
 
-$("button").on("click",function(){
-  return getHotEntry();
-});
+// Sort by hateb count
+function sortByHatebAsc(){
+  $("ul.feeds").eachsort(function(a,b){
+    return +$(b).attr("data-hateb") - +$(a).attr("data-hateb");
+  });
+}
 
-/*
-$("button").on("click", function() {
-  return getHotEntry();
-});
-*/
+// Sort by hateb count
+function sortByHatebDesc(){
+  $("ul.feeds").eachsort(function(a,b){
+    return +$(a).attr("data-hateb") - +$(b).attr("data-hateb");
+  });
+}
+
+function addEvent(){
+
+  var clickEventManager = [
+    // Reload XML
+    {
+      "selector" : "button#reload",
+      "func" : function(){ getHotEntry(); }
+    },
+    // Sort by hatena bookmark count asc.
+    {
+      "selector" : "button#hateb-sort-asc",
+      "func" : function(){ sortByHatebAsc(); }
+    },
+    // Sort by hatena bookmark count desc.
+    {
+      "selector" : "button#hateb-sort-desc",
+      "func" : function(){ sortByHatebDesc(); }
+    },
+  ];
+
+  // Use deligate
+  for( var i=0, iz = clickEventManager.length; i<iz; i++ ){
+    (function(ce){
+      $(document).on("click", ce.selector, ce.func );
+    })(clickEventManager[i]);
+  }
+
+}
+
+
+/* Process */
+function processCode(){
+  getLog();
+  getHotEntry();
+
+  addEvent();
+}
+
+processCode();
