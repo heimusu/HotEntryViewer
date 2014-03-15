4d3c664f357faf04bfc4ce6b61fc9b36d07af89d
heimusu <heimusu@gmail.com>
2014-03-15 17:02:26 +0900

    version 1.2

--- javascripts/popup.js
+++ javascripts/popup.js
@@ -8,14 +8,17 @@ getHotEntry = function() {
     dataType: "xml",
     success: function(data) {
       return $(data).find("item").each(function() {
-        var title;
-        var link;
-	link = $(this).find("link").text();
-        title = $(this).find("title").text();
+        var title=$(this).find("title").text();
+        var link= $(this).find("link").text();
+	var hateb="";
+	$(this).children().each(function(){
+	    if($(this)[0].tagname == "hatena:bookmarkcount"){
+		hateb = $(this).text();
+	    }
+	});
 	//return $(".feeds").append("<p>" + title + "</p>");
-        $("#article-tmpl").tmpl({link: link, title: title}).appendTo(".feeds");
-        $("a:last").on("click", createTab);
-	alert(count);
+        $("#article-tmpl").tmpl({link: link, title: title,hateb: hateb}).appendTo(".feeds");
+        //$("a:last").on("click", createTab);
       });
     }
   });
@@ -39,16 +42,72 @@ createTab = function(event){
 };
 
 
-/*実行部*/
-getLog();
-getHotEntry();
 
-$("button").on("click",function(){
-  return getHotEntry();
-});
 
-/*
-$("button").on("click", function() {
-  return getHotEntry();
-});
-*/
+$.fn.eachsort = function(callback){
+    return this.each(function(){
+	return $(this).html(
+	    $(this).children().sort(callback)
+	);
+    });
+}
+
+function sortByHatebAsc(){
+    $("ul.feeds").eachsort(function(a,b){
+	return +$(b).attr("data-hateb") - +$(a).attr("data-hateb");
+    });
+}
+
+function sortByHatebDesc(){
+    $("ul.feeds").eachsort(function(a,b){
+	return +$(a).attr("data-hateb") - +$(b).attr("data-hateb");
+    });
+}
+
+function addEvent(){
+    
+    //Reload XML
+    var clickEventManager = [{
+	"selector":"button#reload",
+	"func":function(){getHotEntry();}
+    },
+
+    //Sort by hatena book mark asc
+    {
+	"selector":"button#hateb-sort-asc",
+	"func":function(){sortByHatebAsc();}
+    },
+
+    //Sort by hatena book mark desc
+    {
+	"selector":"button#hateb-sort-desc",
+	"func":function(){sortByHatebDesc();}
+    },
+
+    //Open Tab
+    {
+	"selector":"ul.feeds a",
+	"func" : function(){
+	    var url=$(this).attr("href");
+	    createTab(url);
+	    }
+	}
+    ];
+    
+    //use deligate
+    for(var i=0,iz=clickEventManager.length;i<iz;i++){
+	(function(ce){
+	    $(document).on("click",ce.selector,ce.func);
+	})(clickEventManager[i]);
+    }
+}
+
+/*Process*/
+function processCode(){
+    getLog();
+    getHotEntry();
+
+    addEvent();
+}
+
+processCode();
